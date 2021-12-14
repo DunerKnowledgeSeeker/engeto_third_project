cf3d9daecd6c5d303bdc035f6a1ea1a245c6d9ce
Duner <belormarok062@gmail.com>
2021-12-14 23:40:02 +0100

    add get_all_data func

--- func.py
+++ func.py
@@ -5,13 +5,12 @@ from pprint import pprint
 # maybe import sys for sys.argv - according to tak
 
 url = 'https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2103'
+test_url= "https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=532908&xvyber=2103"
 
 
 def main():
     cities_links = get_cities_url(url)
-    print(cities_links)
     location_codes_list = get_location_code(url)
-    print(location_codes_list)
 
 
 
@@ -49,9 +48,42 @@ def get_location_code(district_url):
     return codes
 
 
-# get data from links
+def get_vote_data(city_url):
+    try:
+        r = requests.get(city_url)
+    except requests.exceptions.HTTPError as err:
+        raise exit(err)
+    soup = bs(r.text, "html.parser")
+
     # get location
-    # get registred, envelopes, valid + political parties = HEADERS
+    location = soup.find_all({"h3"})[2]
+    format_name = location.text.split(":")
+    location_name = format_name[1].strip(" \n")
+
+    # get registered, envelopes, valid
+    registered = soup.find("td", {"class": "cislo", "headers": "sa2", "data-rel": "L1"})
+    envelopes = soup.find("td", {"class": "cislo", "headers": "sa3", "data-rel": "L1"})
+    valid = soup.find("td", {"class": "cislo", "headers": "sa6", "data-rel": "L1"})
+
+    # get political parties -> dict - key = name, value = number of votes
+    parties_names = []
+    parties_votes = []
+    for name in soup.find_all("td", {"class": "overflow_name", "headers": "t2sa1 t2sb2"}):
+        parties_names.append(name.text)
+    for vote in soup.find_all("td", {"class": "cislo", "headers": "t2sa2 t2sb3"}):
+        parties_votes.append(vote.text)
+    print(parties_votes)
+    print(parties_names)
+
+
+    return location_name, registered.text, envelopes.text, valid.text
+get_vote_data(test_url)
+
+
+# get political parties = HEADERS
+# create generator with get_vote_data func - arguments = list(url)
+# add location code to csv = extra function
+# from func get_code_data and get_vote_data = create csv
 # save to csv
 
 if __name__ == '__main__':
