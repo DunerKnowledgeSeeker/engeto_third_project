481af8c4ff95005662971092c6938a5412b4c9a9
Duner <belormarok062@gmail.com>
2021-12-15 21:57:32 +0100

    add main func, sys.argv

---
+++ election_scraper_main.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    save_csv = sys.argv[2]
+    main()
+else:
+    print("RUNNING FILE REQUIRED ENTRY URL AND RUNNING THE FILE REQUIRES THE ENTRY URL AND FILE NAME WHERE YOU WANT "
+          "TO SAVE THE DATA")
