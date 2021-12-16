e55ad82d027914725b2254e248d7c85a49dbf442
Duner <martin.sedlak777@gmail.com>
2021-12-16 21:03:25 +0100

    tset

--- save_csv_func.py
+++ save_csv_func.py
@@ -1,1 +1,4 @@
-import scra
+import csv
+
+url = 'https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2103'
+test_url= "https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=532908&xvyber=2103"
